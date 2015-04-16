65b1af081fde364153e48b98ca33cd5aa730fc63
Felipe Theodoro <felipetheodoro@Felipes-iMac.local>
2015-04-16 11:55:46 -0300

    first commit

--- AlarmeCountrySide/ViewController.swift
+++ AlarmeCountrySide/ViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+import MessageUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MFMessageComposeViewControllerDelegate {
+    
+    private let textMessageRecipients = ["000000"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onActiveButtonPressed(sender: UIButton) {
+        self.sendSMSWithText("000")
+    }
+    @IBAction func onDeactiveButtonPressed(sender: UIButton) {
+        self.sendSMSWithText("000")
+    }
+    @IBAction func onInverterButtonPressed(sender: UIButton) {
+        self.sendSMSWithText("000")
+    }
+    
+    func sendSMSWithText(text:String) {
+        if MFMessageComposeViewController.canSendText() {
+            var messageComposeVC = MFMessageComposeViewController()
+            messageComposeVC.messageComposeDelegate = self
+            messageComposeVC.recipients = textMessageRecipients
+            messageComposeVC.body = text
+            
+            self.presentViewController(messageComposeVC, animated: true, completion: nil)
+        }
+    }
+    
+    func messageComposeViewController(controller: MFMessageComposeViewController!, didFinishWithResult result: MessageComposeResult) {
+        switch (result.value) {
+            case MessageComposeResultCancelled.value:
+                println("Message was cancelled")
+                self.dismissViewControllerAnimated(true, completion: nil)
+            case MessageComposeResultFailed.value:
+                println("Message failed")
+                self.dismissViewControllerAnimated(true, completion: nil)
+            case MessageComposeResultSent.value:
+                println("Message was sent")
+                self.dismissViewControllerAnimated(true, completion: nil)
+            default:
+                break;
+        }
+    }
+    
 }
-
